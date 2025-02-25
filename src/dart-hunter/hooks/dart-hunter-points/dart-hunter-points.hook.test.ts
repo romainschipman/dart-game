077aa38175f17caeb57d686d99a8eb2e3ff351d2
rschipman <pro.romain.schipman@gmail.com>
2025-02-25 22:54:34 +0100

    Remove unused 'waitFor' import from test file
    The 'waitFor' import was not being used in the test file and has been removed to clean up the code. This helps improve readability and maintainability of the test suite.

--- src/dart-hunter/hooks/dart-hunter-points/dart-hunter-points.hook.test.ts
+++ src/dart-hunter/hooks/dart-hunter-points/dart-hunter-points.hook.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, waitFor } from "@testing-library/react";
+import { renderHook } from "@testing-library/react";
 import { useDartPoints } from "../../../common/hooks";
 import { calculateHunterScores } from "../../utils";
 import { useDartHunterPoints } from "./dart-hunter-points.hook";
