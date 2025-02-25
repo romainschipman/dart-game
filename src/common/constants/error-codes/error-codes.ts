ad40efab1c1b3e0a93d315bb90833436d3056317
rschipman <pro.romain.schipman@gmail.com>
2025-02-25 19:07:39 +0100

    Add new error codes: UNEXPECTED_ERROR and NO_SCORES_TO_REMOVE
    Extended the ErrorCode type and ERROR_CODES map to include two new cases: UNEXPECTED_ERROR and NO_SCORES_TO_REMOVE. These additions help handle specific error scenarios more explicitly.

--- src/common/constants/error-codes/error-codes.ts
+++ src/common/constants/error-codes/error-codes.ts
@@ -1,6 +1,6 @@
 import { StatusCode } from "../../interfaces";
 
-export type ErrorCode = "INVALID_DART_NOTATION" | "UNDO_LIMIT_REACHED";
+export type ErrorCode = "INVALID_DART_NOTATION" | "UNDO_LIMIT_REACHED" | "UNEXPECTED_ERROR" | "NO_SCORES_TO_REMOVE";
 
 const ERROR_CODES : Record<ErrorCode, StatusCode> = {
     INVALID_DART_NOTATION: {
@@ -13,6 +13,16 @@ const ERROR_CODES : Record<ErrorCode, StatusCode> = {
         code: "UNDO_LIMIT_REACHED",
         message: "Undo limit reached",
     },
+    UNEXPECTED_ERROR: {
+        type: "error",
+        code: "UNEXPECTED_ERROR",
+        message: "Unexpected error",
+    },
+    NO_SCORES_TO_REMOVE: {
+        type: "error",
+        code: "NO_SCORES_TO_REMOVE",
+        message: "No scores to remove"
+    }
 };
 
 export { ERROR_CODES };
