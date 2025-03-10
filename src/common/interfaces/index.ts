b61a9284f30f3678076bed8d68b1f552a57252e1
rschipman <pro.romain.schipman@gmail.com>
2025-03-10 10:23:39 +0100

    Add export for Config interface in index.ts
    The Config interface is now exported from the index file to ensure it can be reused across the project. This change improves the organization and accessibility of shared types.

--- src/common/interfaces/index.ts
+++ src/common/interfaces/index.ts
@@ -1,3 +1,4 @@
 export type { GameState } from "./game-state.interface";
 export type { StatusCode } from "./status-code.interface";
 export type { ParsedScore } from "./parsed-score.interface";
+export type { Config } from "./config.interface";
